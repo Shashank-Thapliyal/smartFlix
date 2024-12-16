2046640215b54444b40b3d6bbe0a836831db15e2
Shashank Thapliyal <shashank151002@gmail.com>
2024-12-16 22:33:10 +0530

    project setup with tailwind + vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+ 
+  return (
+    <>
+      <div className='text-green-800'>
+        HELLO!
+        </div>
+    </>
+  )
+}
+
+export default App
